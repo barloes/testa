fe1c9222ca4b0ea426eda6ad8b03855a2d13f3b6
barloes <junhuitee@hotmail.com>
2022-02-26 17:22:02 +0800

    change msg

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ app.secret_key = "super secret key"
 app.debug = True
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 
+
 @app.route("/hello", methods=["GET"])
 def helloWorld():
-    return jsonify({"msg": "HelloWorld"})
+    return jsonify({"msg": "bye"})
+
 
 @app.route("/", methods=["GET"])
 def healthcheck():
-    return Response(response="abcd123", status=200)
+    return Response(response="abcd1234", status=200)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
